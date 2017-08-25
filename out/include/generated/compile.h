6737ac09c6f35d22b48299b748194fbe51041bc8
MrWigleg <ota3300@mail.ru>
2017-08-25 16:42:14 +0300

    First Commit

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Aug 25 15:29:06 EEST 2017"
+#define LINUX_COMPILE_BY "mrwigleg"
+#define LINUX_COMPILE_HOST "mrwigleg-graloock"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
---
+++ out/include/generated/utsrelease.h
@@ -0,0 +1,3 @@
+#define UTS_RELEASE "3.10.103-gb914b0a-dirty"
+#define BUILD_INFO ""
+#define BUILD_FINGERPRINT ""
